9c12f38643289fd6321dd84e53e6976a3ed9ddaf
Kevin Carpio <kevincarpio135@gmail.com>
2020-01-28 16:33:49 -0800

    Edited message

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,5 @@
 public class Greeter {
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("Welcome to CMSI 186");
     }
 }
